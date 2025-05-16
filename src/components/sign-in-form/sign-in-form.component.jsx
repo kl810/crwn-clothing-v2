b161dd48d0f60f2f00ed3b765936a16fbc69599f
Kye-Lee <kyelee.low@gmail.com>
2025-05-16 10:28:19 +0100

    updated authentication styles scss

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -35,7 +35,16 @@ const SignInForm = () => {
             resetFormFields();
 
         } catch (error) {
-
+            switch(error.code) {
+                case 'auth/wrong-password':
+                    alert('incorrect password for email');
+                    break
+                case 'auth/user-not-found':
+                    alert('no user associated with this email');
+                    break
+                default:
+                    console.log(error)
+            }
         }
     }
 
@@ -71,6 +80,7 @@ const SignInForm = () => {
                 <div className="buttons-container">
                     <Button type="submit">Sign In</Button>
                     <Button 
+                        type="button"
                         buttonType="google"
                         onClick={signInWithGoogle}
                     >
