2f619586f05ecbb7837d2115167a5dd47d0ca9a5
Kye-Lee <kyelee.low@gmail.com>
2025-05-18 19:54:48 +0100

    created react context for state management and used an observer: onAuthStatechanged

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -3,7 +3,6 @@ import { createAuthUserWithEmailAndPassword, createUserDocumentFromAuth } from "
 
 import FormInput from "../form-input/form-input.component";
 import Button from "../button/button.component";
-import { Form } from "react-router-dom";
 
 import './sign-up-form.styles.scss';
 
@@ -17,8 +16,6 @@ const defaultFormFields = {
 const SignUpForm = () => {
     const [formFields, setFormFields] = useState(defaultFormFields);
     const { displayName, email, password, confirmPassword } = formFields;
-
-    console.log(formFields)
 
     const resetFormFields = () => {
         setFormFields(defaultFormFields)
@@ -36,7 +33,6 @@ const SignUpForm = () => {
             const { user } = await createAuthUserWithEmailAndPassword(email, password);
 
             await createUserDocumentFromAuth(user, { displayName });
-
             resetFormFields();
 
         } catch (error) {
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -6,6 +6,8 @@ import {
     GoogleAuthProvider,
     createUserWithEmailAndPassword,
     signInWithEmailAndPassword,
+    signOut,
+    onAuthStateChanged,
 } from "firebase/auth";
 import {
     getFirestore,
@@ -76,10 +78,14 @@ export const createAuthUserWithEmailAndPassword = async (email, password) => {
     if (!email || !password ) return;
 
     return await createUserWithEmailAndPassword(auth, email, password);
-}
+};
 
 export const signInAuthUserWithEmailAndPassword = async (email, password) => {
     if (!email || !password ) return;
 
     return await signInWithEmailAndPassword(auth, email, password);
-}
+};
+
+export const signOutUser = async () => await signOut(auth);
+
+export const onAuthStateChangedListener = (callback) => onAuthStateChanged(auth, callback);
