64252ee1af4c6bd62c4e86e4eb4c7d4936f46d57
Kye-Lee <kyelee.low@gmail.com>
2025-05-25 14:35:12 +0100

    migrated user-context and categories-context to redux

--- src/index.js
+++ src/index.js
@@ -1,25 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import reportWebVitals from './reportWebVitals';
 
 import './index.scss';
 import App from './App';
-import { UserProvider } from './contexts/user.context';
-import { CategoriesProvider } from './contexts/categories.context';
 import { CartProvider } from './contexts/cart-context';
+import { store } from './store/store';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <UserProvider>
-        <CategoriesProvider>
-          <CartProvider>
-            <App />
-          </CartProvider>
-        </CategoriesProvider>
-      </UserProvider>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+            <CartProvider>
+              <App />
+            </CartProvider>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/store/user/user.types.js
@@ -0,0 +1,3 @@
+export const USER_ACTION_TYPES = {
+    SET_CURRENT_USER: 'user/SET_CURRENT_USER'  //user - makes it clear which 'store' this is governed by
+};
